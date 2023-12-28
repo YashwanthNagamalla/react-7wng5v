65299100780fb46905be52d023d29e090e0353e2
YashwanthNagamalla <146729835+YashwanthNagamalla@users.noreply.github.com>
2023-12-28 13:01:22 +0530

    Revert "Yashwanth nagamalla/patch 41030"

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,12 @@
-import PropTypes from 'prop-types'
 
-const Header = ({title})=>{
+const Header = ()=>{
   return (
     <div>
       <header>
-        <h1>{title}</h1>
+        <h1>task tracker</h1>
+        
       </header>
     </div>
   )
-}
-
-Header.PropTypes={
-  title:PropTypes.string,
 }
 export default Header;
